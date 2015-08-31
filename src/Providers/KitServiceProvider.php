19b2e7f10d5b47f5f133494e2e49a86f441da55b
Daksh Mehta <daksh@dristal.com>
2015-08-31 17:39:53 +0530

    Syntax error fixed

--- src/Providers/KitServiceProvider.php
+++ src/Providers/KitServiceProvider.php
@@ -54,7 +54,7 @@ class KitServiceProvider extends ServiceProvider {
     	]);	
 
 		$this->publishes([
-        	__DIR__.'/../../resources/views' => base_path('resources/views/vendor/kit'),,
+        	__DIR__.'/../../resources/views' => base_path('resources/views/vendor/kit'),
     	]);	
 	}
 
